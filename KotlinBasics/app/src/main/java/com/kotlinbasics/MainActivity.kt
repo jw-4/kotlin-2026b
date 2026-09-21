1609b0b06a9b9f3af45b970808815753685a6dd8
jwtest <jw@example.com>
2026-09-21 12:30:06 +0900

    kotlin basics v0.6

--- KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -45,6 +45,16 @@ private fun week04Collections() {
     mutableFruits.add("banana")
     println("Mutable fruits: $mutableFruits")
 
+    val scores = mapOf("Kim" to 100, "Park" to 97, "Lee" to 99)
+    println("Scores: $scores")
+
+    for(fruit in mutableFruits){
+        println("I like $fruit")
+    }
+
+        scores.forEach{(name, score) -> println("$name scored $score")}
+        fruits.forEach { fruit -> println("$fruit") }
+
 }
     private fun week04Classes() {
 
